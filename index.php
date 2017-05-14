ed4acf026629f2b15b076a9966e615d67849b8f0
Jean9611 <jruizg96.11@gmail.com>
2017-05-14 09:24:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-  echo "<h1>Final Laboratorio Sistemas Operativos 1</h1>";
-  echo "<h3>Juan Pablo Ruiz Guerra, 201403914</h3>";
-?>
+<html>
+<body background="Paisaje-Natural.jpg">
+<br/><br/><br/><br/><br/><br/><br/>
+  <div align="center">
+    <?php
+      echo "<h1>Final Laboratorio Sistemas Operativos 1</h1>";
+      echo "<h3>Juan Pablo Ruiz Guerra, 201403914</h3>";
+    ?>
+  </div>
+
+
+</body>
+</html>
